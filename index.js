98085b3ecf5be3f12d06eec13cf88dcd7dd7e677
Sebuliba-Adrian <adrian.sebuliba1@gmail.com>
2019-11-07 10:18:32 +0300

    Setup mongodb schema

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
 import express from 'express';
 import routes from './src/routes/crmRoutes';
 import 'dotenv/config';
+import mongoose  from 'mongoose';
 
 const app = express();
 const PORT = 4000;
 
+//mongoose connection
+mongoose.Promise = global.Promise
+mongoose.connect('mongodb://localhost/CRMdb', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
+//middleware setup
+app.use(express.json())
 routes(app);
 
 app.get('/', (req, res) => res.send(`Node and express server running on PORT ${PORT}`))
