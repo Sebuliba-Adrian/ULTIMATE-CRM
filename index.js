48a14104d6c16cfe79b053a3f29c840c2c43cd07
Sebuliba-Adrian <adrian.sebuliba1@gmail.com>
2019-11-07 09:00:35 +0300

    Add basic routing

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 import express from 'express';
+import routes from './src/routes/crmRoutes';
 import 'dotenv/config';
 
 const app = express();
 const PORT = 4000;
+
+routes(app);
 
 app.get('/', (req, res) => res.send(`Node and express server running on PORT ${PORT}`))
 
--- src/routes/crmRoutes.js
+++ src/routes/crmRoutes.js
@@ -1,1 +1,17 @@
- 
+ const routes = (app) => {
+    app.route('/contact')
+        .get((req, res) => 
+        res.send('GET request sucessful!'))
+
+        .post((req, res) =>
+        res.send('POST request sucessful!'))
+    
+    app.route('/contact/:contactID')
+        .put((req, res)=>
+        res.send('PUT request sucessful!'))
+        
+        .delete((req, res) =>
+        res.send('DELETE requst successful!'))
+ }
+
+ export default routes;
